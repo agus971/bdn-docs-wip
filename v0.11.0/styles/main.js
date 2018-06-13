5684d109782eabd4ef8d092e252b6f30e89cd328
Andrey Akinshin <andrey.akinshin@jetbrains.com>
2018-06-13 19:26:59 +0300

    ++

--- v0.11.0/styles/main.js
+++ v0.11.0/styles/main.js
@@ -3,7 +3,7 @@ $(function () {
         if( $('#navbar-versions').length == 0)
         {
             $('#navbar ul').append('<li id="navbar-versions"><li class="dropdown">\n' +
-                '  <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Version: master <span class="caret"></span></a>\n' +
+                '  <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Version: v0.11.0 <span class="caret"></span></a>\n' +
                 '  <ul class="dropdown-menu">\n' +
                 '    <li><a href="/bdn-docs-wip/">master</a></li>\n' +
                 '    <li><a href="/bdn-docs-wip/v0.11.0/">v0.11.0</a></li>\n' +
