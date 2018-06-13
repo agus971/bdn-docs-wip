d42832ff00eb1e60cc026c173b3c6a12cfef6ce8
Andrey Akinshin <andrey.akinshin@jetbrains.com>
2018-06-13 19:22:22 +0300

    ++

--- styles/main.js
+++ styles/main.js
@@ -1,19 +1,17 @@
 $(function () {
     function updateVersions() {
-        if( $('#breadcrumb-versions').length == 0)
+        if( $('#navbar-versions').length == 0)
         {
-            $('#breadcrumb ul').prepend('<li id="breadcrumb-versions"><div class="btn-group">\n' +
-                '    <button type="button" class="btn btn-default btn-sm dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">\n' +
-                '        BenchmarkDotNet: master <span class="caret"></span>\n' +
-                '    </button>\n' +
-                '    <ul class="dropdown-menu">\n' +
-                '        <li><a href="/bdn-docs-wip/v0.10.14/">v0.10.14</a></li>\n' +
-                '        <li><a href="/bdn-docs-wip/v0.10.13/">v0.10.13</a></li>\n' +
-                '        <li><a href="/bdn-docs-wip/v0.10.12/">v0.10.12</a></li>\n' +
-                '    </ul>\n' +
-                '</div></li>');
+            $('#navbar ul').append('<li id="navbar-versions"><li class="dropdown">\n' +
+                '  <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Version: master <span class="caret"></span></a>\n' +
+                '  <ul class="dropdown-menu">\n' +
+                '    <li><a href="/bdn-docs-wip/v0.10.14/">v0.10.14</a></li>\n' +
+                '    <li><a href="/bdn-docs-wip/v0.10.13/">v0.10.13</a></li>\n' +
+                '    <li><a href="/bdn-docs-wip/v0.10.12/">v0.10.12</a></li>\n' +
+                '  </ul>\n' +
+                '</li></li>');
         }
     }
     updateVersions();
-    $('#breadcrumb').bind("DOMSubtreeModified", updateVersions);
+    $('#navbar').bind("DOMSubtreeModified", updateVersions);
 });
