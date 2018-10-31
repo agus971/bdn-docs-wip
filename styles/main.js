2e7fdf3689bd9f0794d418ea214fd54d0abab417
Andrey Akinshin <andrey.akinshin@gmail.com>
2018-10-31 22:52:41 +0300

    ++

--- styles/main.js
+++ styles/main.js
@@ -2,16 +2,14 @@ $(function () {
     function updateVersions() {
         if( $('#navbar-versions').length == 0)
         {
-            $('#navbar ul').append('<li id="navbar-versions"><li class="dropdown">\n' +
+            $('#navbar ul').append('<li id="navbar-versions" class="dropdown">\n' +
                 '  <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Version: master <span class="caret"></span></a>\n' +
                 '  <ul class="dropdown-menu">\n' +
-                '    <li><a href="/bdn-docs-wip/">master</a></li>\n' +
-                '    <li><a href="/bdn-docs-wip/v0.11.0/">v0.11.0</a></li>\n' +
-                '    <li><a href="/bdn-docs-wip/v0.10.14/">v0.10.14</a></li>\n' +
-                '    <li><a href="/bdn-docs-wip/v0.10.13/">v0.10.13</a></li>\n' +
-                '    <li><a href="/bdn-docs-wip/v0.10.12/">v0.10.12</a></li>\n' +
+                '    <li><a href="/master/">master</a></li>\n' +
+                '    <li><a href="/v0.11.0/">v0.11.0</a></li>\n' +
+                '    <li><a href="/v0.10.14/">v0.10.14</a></li>\n' +
                 '  </ul>\n' +
-                '</li></li>');
+                '</li>');
         }
     }
     updateVersions();
